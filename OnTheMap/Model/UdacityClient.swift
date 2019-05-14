c8d0f9597f8140ce807ffd9a26c4e809c679d138
Sarah Estrellado <sarah.estrellado@icloud.com>
2019-05-14 18:30:24 -0500

    moved UdacityClient error handling to extension function

--- OnTheMap/Model/UdacityClient.swift
+++ OnTheMap/Model/UdacityClient.swift
@@ -13,41 +13,41 @@ class UdacityClient {
     var userId: String? = nil
     
     //MARK: Get
-    func taskForGet(completion: @escaping (_ result: User?, _ error: Error?) -> Void) {
+    func taskForGet(completion: @escaping (_ result: AnyObject?, _ error: Error?) -> Void) {
         let url = URL(string: UdacityConstants.URLConstants.PublicUserData + userId!)
-        print("GET USER DATA URL: " + url!.absoluteString)
         var request = URLRequest(url: url!)
         request.httpMethod = "GET"
         
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            
-            func sendError(_ error: String) {
-                print(error)
-                let userInfo = [NSLocalizedDescriptionKey : error]
-                completion(nil, NSError(domain: "taskForGet", code: 1, userInfo: userInfo))
-            }
-            
-            
-            guard (error == nil) else {
-                sendError("\(error!)")
+            if error != nil {
+                self.sendError("Something went wrong, please try again", "taskForGet", completion)
                 return
             }
             
-            guard data != nil else {
-                sendError("No data")
+            guard let httpStatusCode = (response as? HTTPURLResponse)?.statusCode else {
+                // Since there is no HTTP StatusCode (which will bnever happen). Adding it just to safely
+                // unwrap the StatusCode
+                self.sendError("Something went wrong, please try again", "taskForGet", completion)
                 return
             }
             
-            let newData = data?.subdata(in: 5..<data!.count)
-            if let json = try? JSONSerialization.jsonObject(with: newData!, options: []),
-                let dict = json as? [String:Any],
-                let firstName = dict[UdacityConstants.JSONResponseKeys.firstName] as? String,
-                let lastName = dict[UdacityConstants.JSONResponseKeys.lastName] as? String {
-                
-                let user = User(firstName: firstName, lastName: lastName)
-                completion(user , nil)
-            } else {
-                sendError("Couldn't parse response")
+            if httpStatusCode >= 200 && httpStatusCode < 300 {
+                // Since Status Code is valid. Process Data here only.
+                // This is syntax to create Range in Swift 5
+                let newData = data?.subdata(in: 5..<data!.count)
+                if let json = try? JSONSerialization.jsonObject(with: newData!, options: []),
+                    let dict = json as? [String:Any],
+                    let firstName = dict[UdacityConstants.JSONResponseKeys.firstName] as? String,
+                    let lastName = dict[UdacityConstants.JSONResponseKeys.lastName] as? String {
+                    
+                    let user = User(firstName: firstName, lastName: lastName)
+                    completion(user as AnyObject , nil)
+                } else {
+                    self.sendError("Couldn't parse response", "taskForGet", completion)
+                }
+            }
+            else{
+                self.sendErrorForHttpStatusCode(httpStatusCode, "taskForGet", completion)
             }
         }
         task.resume()
@@ -65,45 +65,46 @@ class UdacityClient {
         request.httpBody = "{\"udacity\": {\"username\": \"\(username)\", \"password\": \"\(password)\"}}".data(using: .utf8)
         
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            func sendError(_ error: String) {
-                print("Error logging in: " + error)
-                let userInfo = [NSLocalizedDescriptionKey : error]
-                completion(nil, NSError(domain: "taskForPost", code: 1, userInfo: userInfo))
-            }
-            
-            guard (error == nil) else {
-                sendError("\(error!)")
+            if error != nil {
+                self.sendError("Something went wrong, please try again", "taskForPost", completion)
                 return
             }
             
-            
-            guard data != nil else {
-                sendError("No data")
+            guard let httpStatusCode = (response as? HTTPURLResponse)?.statusCode else {
+                // Since there is no HTTP StatusCode (which will bnever happen). Adding it just to safely
+                // unwrap the StatusCode
+                self.sendError("Something went wrong, please try again", "taskForPost", completion)
                 return
             }
             
-            let newData = data?.subdata(in: 5..<data!.count)
-            if let json = try? JSONSerialization.jsonObject(with: newData!, options: []),
-                let dict = json as? [String:Any],
-                let sessionDict = dict["session"] as? [String: Any],
-                let accountDict = dict["account"] as? [String: Any]  {
-                
-                let key = accountDict["key"] as? String
-                self.userId = key
-                completion(key as AnyObject, nil)
-                
-            } else {
-                let errString = "Couldn't parse response"
-                let error = [NSLocalizedDescriptionKey : errString]
-                completion(nil, NSError(domain: "taskForPost", code: 1, userInfo: error))
+            if httpStatusCode >= 200 && httpStatusCode < 300 {
+                // Since Status Code is valid. Process Data here only.
+                // This is syntax to create Range in Swift 5
+                let range = 5..<data!.count
+                let newData = data?.subdata(in: range) /* subset response data! */
+                // Continue processing the data and deserialize it
+                if let json = try? JSONSerialization.jsonObject(with: newData!, options: []),
+                    let dict = json as? [String:Any],
+                    let sessionDict = dict["session"] as? [String: Any],
+                    let accountDict = dict["account"] as? [String: Any]  {
+                    
+                    let key = accountDict["key"] as? String
+                    self.userId = key
+                    completion(key as AnyObject, nil)
+                    
+                } else {
+                    self.sendError("Couldn't parse response", "taskForPost", completion)
+                }
             }
-            
+            else{
+                self.sendErrorForHttpStatusCode(httpStatusCode, "taskForPost", completion)
+            }
         }
         task.resume()
     }
     
     //MARK: Delete
-    func taskForDelete (completion: @escaping (_ result: String?, _ error: NSError?) -> Void)  {
+    func taskForDelete (completion: @escaping (_ result: AnyObject?, _ error: NSError?) -> Void)  {
         
         let url = URL(string: UdacityConstants.URLConstants.PublicUserURL)
         var request = URLRequest(url: url!)
@@ -119,41 +120,31 @@ class UdacityClient {
         }
         
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            func sendError(_ error: String) {
-                print(error)
-                let userInfo = [NSLocalizedDescriptionKey : error]
-                completion(nil, NSError(domain: "taskForDelete", code: 1, userInfo: userInfo))
-            }
-            
-            guard (error == nil) else {
-                sendError("\(error!)")
+            if error != nil {
+                self.sendError("Something went wrong, please try again", "taskForDelete", completion)
                 return
             }
             
-            guard data != nil else {
-                sendError("No data")
+            guard let httpStatusCode = (response as? HTTPURLResponse)?.statusCode else {
+                // Since there is no HTTP StatusCode (which will bnever happen). Adding it just to safely
+                // unwrap the StatusCode
+                self.sendError("Something went wrong, please try again", "taskForDelete", completion)
                 return
             }
             
-            let range = Range(5..<data!.count)
-            let newData = data?.subdata(in: range) /* subset response data! */
-            completion(String(data: newData!, encoding: .utf8), nil)
+            if httpStatusCode >= 200 && httpStatusCode < 300 {
+                // Since Status Code is valid. Process Data here only.
+                // This is syntax to create Range in Swift 5
+                let range = 5..<data!.count
+                let newData = data?.subdata(in: range) /* subset response data! */
+                // Continue processing the data and deserialize it
+                completion(String(data: newData!, encoding: .utf8) as AnyObject, nil)
+            }
+            else{
+                self.sendErrorForHttpStatusCode(httpStatusCode, "taskForDelete", completion)
+            }
         }
         task.resume()
-    }
-    
-    //MARK: Convert
-    public func convertDataWithCompletionHandler(_ data: Data, completion: (_ result: AnyObject?, _ error: NSError?) -> Void) {
-        
-        var parsedResult: AnyObject! = nil
-        do {
-            parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as AnyObject
-        } catch {
-            let userInfo = [NSLocalizedDescriptionKey : "Could not parse the data as JSON: '\(data)'"]
-            completion(nil, NSError(domain: "convertDataWithCompletionHandler", code: 1, userInfo: userInfo))
-        }
-        
-        completion(parsedResult, nil)
     }
     
     //MARK: Shared
--- OnTheMap/Model/UdacityClientSupport.swift
+++ OnTheMap/Model/UdacityClientSupport.swift
@@ -28,7 +28,7 @@ extension UdacityClient {
             if let error = error {
                 completion(nil, error as NSError)
             } else {
-                completion(user, nil)
+                completion(user as! User, nil)
             }
         }
         
@@ -45,6 +45,28 @@ extension UdacityClient {
             }
         }
         
+    }
+    
+    func sendError(_ errorString: String, _ domain: String, _ completion: @escaping (_ result: AnyObject?, _ error: NSError?) -> Void) {
+        let userInfo = [NSLocalizedDescriptionKey : errorString]
+        completion(nil, NSError(domain: domain, code: 1, userInfo: userInfo))
+    }
+    
+    func sendErrorForHttpStatusCode(_ httpStatusCode: Int, _ domain: String, _ completion: @escaping (_ result: AnyObject?, _ error: NSError?) -> Void) {
+        switch(httpStatusCode) {
+        case 400: // BadRequest
+            sendError("Bad request, please try again", domain, completion)
+        case 401: // Invalid Credentials
+            sendError("Invalid Credentials, please try again", domain, completion)
+        case 403: // Invalid Credentials
+            sendError("Unauthorized, please try again", domain, completion)
+        case 410: // URL Changed
+            sendError("URL changed, please try again", domain, completion)
+        case 500: // URL Changed
+            sendError("Server error, please try again later", domain, completion)
+        default:
+            sendError("Something went wrong, please try again", domain, completion)
+        }
     }
 }
 
